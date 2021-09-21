c1f503b3bd01059739d9d7a3340d5120323d5a33
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-09-21 17:25:21 -0600

    Quiz start button

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var DisTime = document.getElementById("time");
-var Quiz = document.querySelector(".Test")
+Show = document.querySelector(".hide")
+Show1 = document.querySelector(".hide1")
+// go=Show.setAttribute('class', 'position')
+// go1=Show1.setAttribute('class', 'Test')
+DisTime = document.getElementById("time");
+Quiz = document.querySelector(".Test")
 
 
 question= document.getElementById("Questions")
@@ -11,30 +15,33 @@ answer2=document.getElementById("answer2")
 answer3=document.getElementById("answer3")
 answer4=document.getElementById("answer4")
 
-state1=answer1.getAttribute("data-1")
-answer1.textContent=state1
+answer1.textContent=answer1.getAttribute("data-1")
 
-state2=answer2.getAttribute("data-1")
-answer2.textContent=state2
+answer2.textContent=answer2.getAttribute("data-1")
 
-state3=answer3.getAttribute("data-1")
-answer3.textContent=state3
+answer3.textContent=answer3.getAttribute("data-1")
 
-state4=answer4.getAttribute("data-1")
-answer4.textContent=state4
-var secondsLeft = 10;
+answer4.textContent=answer4.getAttribute("data-1")
+
+var secondsLeft = 90;
 
 Quiz.addEventListener('click', function(event){
     tar = event.target
-    state = tar.getAttribute("data-state")
+    state = tar.getAttribute("data-1")
     console.log(state)
-    if(state=="Yes"){
+    if(state=="Booleans"){
         question.textContent=question.getAttribute("data-2")
+        answer1.textContent=answer1.getAttribute("data-2")
+        answer2.textContent=answer2.getAttribute("data-2")
+        answer3.textContent=answer3.getAttribute("data-2")
+        answer4.textContent=answer4.getAttribute("data-2")
     }
     else{
         secondsLeft+=7;
     }
 })
+
+
 
 
 function setTime() {
