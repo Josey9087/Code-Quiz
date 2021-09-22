3d1be6bb240ed4e699f7e0a226ab2ee927687d2d
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-09-22 13:25:22 -0600

    All of the quiz portion is done

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 Start=document.getElementById("start")
 DisTime = document.getElementById("time");
 Quiz = document.getElementById("Test")
+questions1= document.getElementById("Questions")
+WrongRight=document.querySelector(".Opacity")
+end=""
+
 Start.addEventListener('click',function(){
+  setTime()
   start1=document.getElementById("On")
   start2=start1.setAttribute('id', 'hide2')
   Show1 = document.querySelector(".hide1")
@@ -30,29 +35,41 @@ function first(){
 var secondsLeft = 90;
 
 function second(){Quiz.addEventListener('click', function (event){
-    if(questions1==questions1.getAttribute("data-1")){
+  questions1= document.getElementById("Questions")
+    if(questions1.textContent==questions1.getAttribute("data-1")){
       tar = event.target
       state2 = tar.getAttribute("data-1")
       console.log(state2)
       if(state2=="Booleans"){
+        WrongRight.textContent="Correct"
         questions1= document.getElementById("Questions")
         questions1.textContent=questions1.getAttribute("data-2")
         answer1.textContent=answer1.getAttribute("data-2")
         answer2.textContent=answer2.getAttribute("data-2")
         answer3.textContent=answer3.getAttribute("data-2")
         answer4.textContent=answer4.getAttribute("data-2")
-    }}
+    }
     else{
-        secondsLeft+=7;
+        secondsLeft-=10;
+        WrongRight.textContent="Wrong"
+        questions1= document.getElementById("Questions")
+        questions1.textContent=questions1.getAttribute("data-2")
+        answer1.textContent=answer1.getAttribute("data-2")
+        answer2.textContent=answer2.getAttribute("data-2")
+        answer3.textContent=answer3.getAttribute("data-2")
+        answer4.textContent=answer4.getAttribute("data-2")
     }
     third()
-})};
+}})};
 
 function third(){Quiz.addEventListener('click', function question(event){
+  questions1= document.getElementById("Questions")
+  if(questions1.textContent==questions1.getAttribute("data-2")){
   tar = event.target
   state3 = tar.getAttribute("data-2")
   console.log(state3)
   if(state3=="Parentheses"){
+      WrongRight.textContent="Correct"
       questions1= document.getElementById("Questions")
       questions1.textContent=questions1.getAttribute("data-3")
       answer1.textContent=answer1.getAttribute("data-3")
@@ -61,16 +78,27 @@ function third(){Quiz.addEventListener('click', function question(event){
       answer4.textContent=answer4.getAttribute("data-3")
   }
   else{
-      secondsLeft+=7;
+      WrongRight.textContent="Wrong"
+      secondsLeft-=10;
+      questions1= document.getElementById("Questions")
+      questions1.textContent=questions1.getAttribute("data-3")
+      answer1.textContent=answer1.getAttribute("data-3")
+      answer2.textContent=answer2.getAttribute("data-3")
+      answer3.textContent=answer3.getAttribute("data-3")
+      answer4.textContent=answer4.getAttribute("data-3")
   }
-})};
+  fourth()
+}})};
 
 
 function fourth() {Quiz.addEventListener('click', function question(event){
   tar = event.target
+  questions1= document.getElementById("Questions")
+    if(questions1.textContent==questions1.getAttribute("data-3")){
   state4 = tar.getAttribute("data-3")
   console.log(state4)
   if(state4=="All of the above"){
+      WrongRight.textContent="Correct"
       questions1= document.getElementById("Questions")
       questions1.textContent=questions1.getAttribute("data-4")
       answer1.textContent=answer1.getAttribute("data-4")
@@ -79,16 +107,36 @@ function fourth() {Quiz.addEventListener('click', function question(event){
       answer4.textContent=answer4.getAttribute("data-4")
   }
   else{
-      secondsLeft+=7;
+      WrongRight.textContent="Wrong"
+      secondsLeft-=10;
+      questions1= document.getElementById("Questions")
+      questions1.textContent=questions1.getAttribute("data-4")
+      answer1.textContent=answer1.getAttribute("data-4")
+      answer2.textContent=answer2.getAttribute("data-4")
+      answer3.textContent=answer3.getAttribute("data-4")
+      answer4.textContent=answer4.getAttribute("data-4")
   }
   fifth()
-})};
+}})};
 
 function fifth(){ Quiz.addEventListener('click', function question(event){
-  tar = event.target
-  state5 = tar.getAttribute("data-4")
-  console.log(state5)
-  if(state5=="Quotes"){
+  questions1= document.getElementById("Questions")
+    if(questions1.textContent==questions1.getAttribute("data-4")){
+      tar = event.target
+      state5 = tar.getAttribute("data-4")
+      console.log(state5)
+      if(state5=="Quotes"){
+        WrongRight.textContent="Correct"
+        questions1= document.getElementById("Questions")
+        questions1.textContent=questions1.getAttribute("data-5")
+        answer1.textContent=answer1.getAttribute("data-5")
+        answer2.textContent=answer2.getAttribute("data-5")
+        answer3.textContent=answer3.getAttribute("data-5")
+        answer4.textContent=answer4.getAttribute("data-5")
+  }
+  else{
+      WrongRight.textContent="Wrong"
+      secondsLeft-=10;
       questions1= document.getElementById("Questions")
       questions1.textContent=questions1.getAttribute("data-5")
       answer1.textContent=answer1.getAttribute("data-5")
@@ -96,12 +144,37 @@ function fifth(){ Quiz.addEventListener('click', function question(event){
       answer3.textContent=answer3.getAttribute("data-5")
       answer4.textContent=answer4.getAttribute("data-5")
   }
-  else{
-      secondsLeft+=7;
+  sixth()
+}})};
+
+function sixth(){ Quiz.addEventListener('click', function question(event){
+  questions1= document.getElementById("Questions")
+  end="Stop"
+    if(questions1.textContent==questions1.getAttribute("data-5")){
+      tar = event.target
+      state5 = tar.getAttribute("data-5")
+      console.log(state5)
+      if(state5=="Terminal/Bash"){
+        WrongRight.textContent="Correct"
+        end="Stop"
   }
-})};
+  else{
+      WrongRight.textContent="Wrong"
+      secondsLeft-=10;
+      end="Stop"
+  }
+  final()
+}})};
 
+function final(){
+  hide=document.querySelector(".Test")
+  hide1=hide.setAttribute('class', 'hide1')
+  show=document.querySelector(".hide")
+  show1=show.setAttribute('class', 'Show')
+  score=document.querySelector(".score")
+  score.textContent="Your final score is: " +secondsLeft;
 
+}
 
 
 
@@ -116,10 +189,12 @@ function setTime() {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
-      DisTime.textContent=" ";
+      DisTime.textContent="Time: ";
+    }
+    if(end=="Stop"){
+      clearInterval(timerInterval)
+      DisTime.textContent = "Time: ";
     }
 
   }, 1000);
 }
-
-setTime()
