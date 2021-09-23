cfe4f5aa6f408ca62579814580a2783f76b5b89e
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-09-22 23:33:14 -0600

    Comments

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,16 @@
+// Below are global variables that will be used in the code, some are grabbing areas of the html.
 Start=document.getElementById("start")
 DisTime = document.getElementById("time");
 Quiz = document.getElementById("Test")
 questions1= document.getElementById("Questions")
 WrongRight=document.querySelector(".Opacity")
+high=document.getElementById("Highscores")
 end=""
-comment = document.getElementById("msg");
 submit=document.getElementById("save")
 secondsLeft = 90;
 
-
+// This is an event listener for the Start Quiz button, it will activate the timer as well as hide the orginal starting section and begin to display the quiz section.
+// It will finally call the next function in the process.
 Start.addEventListener('click',function(){
   setTime()
   start1=document.getElementById("On")
@@ -18,6 +20,8 @@ Start.addEventListener('click',function(){
   first()
 })
 
+// This function is grabbing elements based on their id. It changes the text content of the sections displaying each question and each answer to
+// data-1. Inside the data attribute is the question and answers that correspond to each other. It calls the next function in the process.
 function first(){
   questions1= document.getElementById("Questions")
   questions1.textContent=questions1.getAttribute("data-1")
@@ -36,7 +40,12 @@ function first(){
   second()
 }
 
-
+// This function is called when a button within the "Test" id is clicked. It is grabbing elements based on their id. 
+// It checks to make sure what question the test is on since there are muliple functions
+// being called by the same button. If the requirment for the order in which the questions are displayed is correct then,
+// it displays if the answer was correct or wrong depending on the answer that was clicked. this is checked through the event target
+// and compared to the answer which is correct. It changes the text content of the sections displaying each question and each answer to
+// data-2. Inside the data attribute is the question and answers that correspond to each other. It calls the next function in the process.
 function second(){Quiz.addEventListener('click', function (event){
   questions1= document.getElementById("Questions")
     if(questions1.textContent==questions1.getAttribute("data-1")){
@@ -65,6 +74,12 @@ function second(){Quiz.addEventListener('click', function (event){
     third()
 }})};
 
+// This function is called when a button within the "Test" id is clicked. It is grabbing elements based on their id. 
+// It checks to make sure what question the test is on since there are muliple functions
+// being called by the same button. If the requirment for the order in which the questions are displayed is correct then,
+// it displays if the answer was correct or wrong depending on the answer that was clicked. this is checked through the event target
+// and compared to the answer which is correct. It changes the text content of the sections displaying each question and each answer to
+// data-3. Inside the data attribute is the question and answers that correspond to each other. It calls the next function in the process.
 function third(){Quiz.addEventListener('click', function question(event){
   questions1= document.getElementById("Questions")
   if(questions1.textContent==questions1.getAttribute("data-2")){
@@ -93,7 +108,12 @@ function third(){Quiz.addEventListener('click', function question(event){
   fourth()
 }})};
 
-
+// This function is called when a button within the "Test" id is clicked. It is grabbing elements based on their id. 
+// It checks to make sure what question the test is on since there are muliple functions
+// being called by the same button. If the requirment for the order in which the questions are displayed is correct then,
+// it displays if the answer was correct or wrong depending on the answer that was clicked. this is checked through the event target
+// and compared to the answer which is correct. It changes the text content of the sections displaying each question and each answer to
+// data-4. Inside the data attribute is the question and answers that correspond to each other. It calls the next function in the process.
 function fourth() {Quiz.addEventListener('click', function question(event){
   tar = event.target
   questions1= document.getElementById("Questions")
@@ -121,7 +141,12 @@ function fourth() {Quiz.addEventListener('click', function question(event){
   }
   fifth()
 }})};
-
+// This function is called when a button within the "Test" id is clicked. It is grabbing elements based on their id. 
+// It checks to make sure what question the test is on since there are muliple functions
+// being called by the same button. If the requirment for the order in which the questions are displayed is correct then,
+// it displays if the answer was correct or wrong depending on the answer that was clicked. this is checked through the event target
+// and compared to the answer which is correct. It changes the text content of the sections displaying each question and each answer to
+// data-5. Inside the data attribute is the question and answers that correspond to each other. It calls the next function in the process.
 function fifth(){ Quiz.addEventListener('click', function question(event){
   questions1= document.getElementById("Questions")
     if(questions1.textContent==questions1.getAttribute("data-4")){
@@ -149,7 +174,12 @@ function fifth(){ Quiz.addEventListener('click', function question(event){
   }
   sixth()
 }})};
-
+// This function is called when a button within the "Test" id is clicked. It is grabbing elements based on their id. 
+// It checks to make sure what question the test is on since there are muliple functions
+// being called by the same button. If the requirment for the order in which the questions are displayed is correct then,
+// it displays if the answer was correct or wrong depending on the answer that was clicked. this is checked through the event target
+// and compared to the answer which is correct. It then sets the variable 'end' to "Stop" this will stop the timer in the another function.
+// It calls the next function in the process.
 function sixth(){ Quiz.addEventListener('click', function question(event){
   questions1= document.getElementById("Questions")
     if(questions1.textContent==questions1.getAttribute("data-5")){
@@ -168,6 +198,8 @@ function sixth(){ Quiz.addEventListener('click', function question(event){
   final()
 }})};
 
+// This function hides the section with the quiz by changing the name of the class, this will hide them or display them in the css.
+// It will set the content of the element that displays your score to the current amount of time left.
 function final(){
   finalseconds=secondsLeft
   hide=document.querySelector(".Test")
@@ -178,7 +210,10 @@ function final(){
   score.textContent="Your final score is: " + finalseconds;
 
 }
-
+// An event listener for the "SAVE" button, it will activate a function.
+// The function will stop the button from deleting the input right away through prevent.Default.
+// It will save the input of your initals and place it into a a variable that contains objects. it will then save the time left.
+// The array of object is saved into the local stoarage as the key "Player" and stringifyed. The next fucntions are called.
 submit.addEventListener("click", function(event) {
   event.preventDefault();
   comment = document.getElementById("initials");
@@ -190,28 +225,35 @@ submit.addEventListener("click", function(event) {
     renderMessage();
     highscore()
   });
-
+// This function uses the parse method to remove the object out of the string that was stringified in a prior function that was saved under the key "Player".
+// It displays to the message class, the initals from the local stoarage and the seconds left that were saved to the storage corresponding with those initals.
 function renderMessage() {
     var Highscore = JSON.parse(localStorage.getItem("Player"));
     document.querySelector(".message").textContent = Highscore.player+":"+ Highscore.score
     
   }
-  function highscore(){
+
+// This function hides the section where you can save your score and displays the section that displays the highscore.
+function highscore(){
   show=document.querySelector(".Show")
   show1=show.setAttribute('class', 'hide2')
   show2=document.querySelector(".hide3")
   show12=show2.setAttribute('class', "Show1")
   }
-
-high=document.getElementById("Highscores")
+  
+// An event listener for the "View Highscore" button. It uses the parse method to remove the object out of the string that was stringified in a prior function and was saved under the key "Player".
+// It displays the highscore which is grabbed from the local storage, as an alert.
 high.addEventListener('click', function(){
   scores=JSON.parse(localStorage.getItem("Player"));
-  alert("The highscore is: " + scores.player+": " + scores.score)
+  alert("The highscore is:   " + scores.player+": " + scores.score)
 })
 
 
 
-
+// This function starts the timer by creating an interval which is set to 1000, this is equal to 1 second.
+// The seconds left is displayed next ot the time.
+// If the timer is equal to or less than 0 it will stop and call the function named final to stop the game even though all questiones aren't answered.
+// If end is equal the "Stop" then the timer will stop and no longer display the time. the variable end becomes equal to stop when all questions are answered.
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Code-Quiz
+
+Website: Code Quiz
+
+This is a Code Quiz project for the University of Denver Coding Bootcamp. I wrote all of the code.
+
+My assignment was as follows:
+
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+
+My Acceptance Criteria was as follows:
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+
+
+Explainations for the code are commented in the code.
+
+![Code Quiz Screenshot](./Images/Code_Quiz.png)
